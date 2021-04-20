18d7653931fbc20b7958236b19203e9513f1b6ec
brialaws98 <brianalaws98@gmail.com>
2021-04-20 00:51:05 -0400

    Include API
    I include API/AJAX from a weather website that provided it (look up "open weather api") so that the weather can be more accurate

--- src/index.js
+++ src/index.js
@@ -1,56 +1,80 @@
 let today = document.querySelector("#current-date");
-let detailToday= document.querySelector("#dateDetail");
-
+let detailToday = document.querySelector("#dateDetail");
 let currentTime = new Date();
 let hours = currentTime.getHours();
- if (hours < 10){
-    hours = `0${hours}`;
- }
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 
-let minutes= currentTime.getMinutes();
- if (minutes < 10){
-     minutes = `0${minutes}`
- } 
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let dayIndex = currentTime.getDay();
-let days =[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
 ];
-let month= currentTime.getMonth();
-let months= [
-    "Jan",
-    "Feb",
-    "Mar", 
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sep", 
-    "Oct",
-    "Nov",
-    "Dec"
-]
-let date= currentTime.getDate();
-let year= currentTime.getFullYear();
-today.innerHTML= `${days[dayIndex]}, ${hours}:${minutes}`;
-detailToday.innerHTML= `${months[month]} ${date}, ${year}`;
+let month = currentTime.getMonth();
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let date = currentTime.getDate();
+let year = currentTime.getFullYear();
+today.innerHTML = `${days[dayIndex]}, ${hours}:${minutes}`;
+detailToday.innerHTML = `${months[month]} ${date}, ${year}`;
 
-function search(event){
-    event.preventDefault();
-   let changeCity=document.querySelector("change-city");
-    let apiKey= "f58174ae50eb90059664519b884ea7dc";
-    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${changeCity}&appid=${apiKey}`;
+function displayWeather(response) {
  
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#humidity").innerHTML= response.data.main.humidity;
+  document.querySelector("#wind").innerHTML=Math.round(response.data.wind.speed);
+  document.querySelector("#description").innerHTML=response.data.weather[0].main;
+}
+function searchHome (city){
+  let apiKey = "a1efc4a7356688ae30bb6a1809d1bb99";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchHome(city);
+}
 
-let seachCity= document.querySelector("#search-city");
+let seachCity = document.querySelector("#search-city");
+searchCity.addEventListener("submit", handleSubmit);
 
-searchCity.addEventListener("submit", search);
+function searchLocation(position) {
+  let apiKey = "a1efc4a7356688ae30bb6a1809d1bb99";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+}
 
+function displayCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton=document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", displayCurrentLocation);
+
+  searchHome("Baltimore");
 
